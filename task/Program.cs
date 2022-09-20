1d1c2c78fc8d5e1b46206fee881954d51a17d03f
dmitriyzheleznyak <komenus@gmail.com>
2022-09-20 03:01:55 +0300

    create massive

---
+++ task/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк, которые хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
+string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+    strs[i] = Console.ReadLine(); //Заполняем его
+}
+Console.WriteLine("Вы ввели следующие строки :");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(strs[i]);
+}
+Console.Write("Нажмите [Enter] для выхода...");
+Console.ReadLine();
+// int len = strs[0].Length;
+for (int i = 0; i < n; i++)
+int len = strs[i].Length;
+{
+    if
+}
